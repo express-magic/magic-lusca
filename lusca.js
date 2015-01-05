d15bb1b2affc86588d9fe5ca472a1f593a1350a7
Jascha Ehrenreich <jascha@jaeh.at>
2015-01-06 00:20:22 +0100

    added first version with functionality

--- lusca.js
+++ lusca.js
@@ -1,5 +1,22 @@
 'use strict';
+var utils = require('magic-utils')
+;
+
 //middleware function
 module.exports = function (req, res, next) {
+  var app            = req.app
+    , luscaOptions   = app.get('lusca')
+    , defaultOptions = {}
+  ;
+
+  if ( typeof lusca === 'object' ) {
+    var options = {};
+
+    utils.each(defaultOptions, function (option, key) {
+      options[key] = option;
+    });
+    utils.log('magic-lusca', 'options', options);
+  }
+
   next();
 }
